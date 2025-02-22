0cef7b0ebeb4680bacfaf0a774f37da7426652d4
minh-dev1801 <minhnv1801@gmail.com>
2025-02-22 22:28:27 +0700

    Create cart Prisma and Zod schemas

--- auth.ts
+++ auth.ts
@@ -65,12 +65,34 @@ export const config = {
     any) {
       //Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       //If there is an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
       }
       return session;
+    },
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user, trigger, session }: any) {
+      //Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        //If user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          //update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
